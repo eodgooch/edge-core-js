7255510a71521898b6de18814da2d6bd02db4490
Aaron Gooch <eodgooch@gmail.com>
2018-10-10 17:14:20 -0700

    prettify

--- src/modules/exchange/shapeshift.js
+++ src/modules/exchange/shapeshift.js
@@ -40,7 +40,7 @@ export type ShapeShiftExactQuoteReply = {
 export function makeShapeshiftApi (ai: ApiInput) {
   const io = ai.props.io
   const apiKey = ai.props.shapeshiftKey
-  const bearerToken = ai.props.shapeshiftBearerToken
+  const bearerToken = ai.props.shapeshiftBearerToken || ''
 
   const api = {
     async get (path) {
@@ -64,7 +64,7 @@ export function makeShapeshiftApi (ai: ApiInput) {
         headers: {
           Accept: 'application/json, text/plain, */*',
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${bearerToken}`
+          Authorization: `Bearer ${bearerToken}`
         },
         body: JSON.stringify(body)
       })
